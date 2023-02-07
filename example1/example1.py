2cf0806daea6e205bf7f4789685ec33c6ab37bc4
Guennadi Maximov Cortés <g.maxc.fox@protonmail.com>
2023-02-07 11:42:21 -0600

    Updated: File completed.

--- example1/example1.py
+++ example1/example1.py
@@ -2,13 +2,37 @@
 
 
 import sys
+import numpy as np
 
 import matplotlib.pyplot as plt
 
 
+def f(x: float, a: float, b: float) -> float:
+    """Dynamic System function."""
+    return (a * x) + b
+
+
 def main() -> int:
     """Execute main workflow."""
-    plt.plot([0, 1], [0, 1])
+    # plt.plot([0, 1], [0, 1])
+    # plt.show()
+
+    # Initial Conditions
+    a = 1.
+    b = 2.
+    x_0 = .5
+    N = 100
+
+    i = np.arange(0, N, 1)
+    x = x_0
+    X = list()
+
+    for _ in i:
+        print(x)
+        X.append(x)
+        x = f(x, a, b)
+
+    plt.plot(X)
     plt.show()
 
     return 0
