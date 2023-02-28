6c7f1c2526263ec349e8a4bae75abcb393d969fc
Guennadi Maximov Cortés <g.maxc.fox@protonmail.com>
2023-02-28 11:57:15 -0600

    Exercise correction.

--- 02_28_2022/GMaximov_successions.py
+++ 02_28_2022/GMaximov_successions.py
@@ -9,14 +9,12 @@ import numpy as np
 if __name__ == '__main__':
     S = list()
     SP = list()
-    i = 1.
     a = 1.
 
-    while a:
+    for i in np.arange(1, 11, 1):
         a = math.pow(10., -i)
         S.append(5 + a)
-        SP.append('${5 + ' + "\\" + 'frac{1}{' + f'{a}' + '}}$')
-        i += 1.
+        SP.append('5 + ' + "\\" + 'frac{1}{10^{' + f'{i}' + '}}')
 
     print(S)
     print(SP)
